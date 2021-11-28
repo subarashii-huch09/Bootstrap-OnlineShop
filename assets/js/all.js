86b654fab2eda8b3f97e7802214944c677695e10
Tansy <cwhu823@icloud.com>
2021-11-29 11:12:02 +1300

    Update 2021-11-28T22:11:58.342Z

---
+++ assets/js/all.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function () {
+  $('#removeModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // 選取當初觸發事件的按鈕
+
+    var title = button.data('title'); // 使用 data-* 來取得特定內容
+
+    var modal = $(this);
+    modal.find('.modal-title').text('確認刪除 ' + "" + title); // 寫入資料
+  });
+});
+//# sourceMappingURL=all.js.map
