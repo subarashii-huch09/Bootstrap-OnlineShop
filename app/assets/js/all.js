1a7c9e1f87417eb0bf696acb4742f8b274d710d0
Tansy <cwhu823@icloud.com>
2021-11-29 10:54:40 +1300

    submit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,8 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
+$(document).ready(function () {
+      $('#removeModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // 選取當初觸發事件的按鈕
+        var title = button.data('title') // 使用 data-* 來取得特定內容
+        var modal = $(this)
+        modal.find('.modal-title').text('確認刪除 ' + "" + title) // 寫入資料
+      })
+    });
